c4bb60111ba5230c9b8eb6d74d7b20790e054cb5
roxydr <ishikasadhukhan90@gmail.com>
2025-10-31 00:34:54 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,22 @@ const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+// app.use(cors({
+//     origin: "https://recrave-d5ek.vercel.app",
+// }));
+
+
 app.use(cors({
-    origin: "https://recrave-d5ek.vercel.app",
+  origin: [
+    "http://localhost:5173",
+    "https://recrave-d5ek.vercel.app",
+    "https://recrave.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
+
 
 // app.use(express.static(path.join(__dirname, 'project/dist')));
 // app.get(/.*/, (req, res) => {
