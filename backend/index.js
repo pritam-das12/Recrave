baab1570047125a258ff9f032c9d764244418703
roxydr <ishikasadhukhan90@gmail.com>
2025-10-31 01:11:47 +0530

    path updated

--- backend/index.js
+++ backend/index.js
@@ -15,35 +15,17 @@ const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// app.use(cors({
-//     origin: "https://recrave-d5ek.vercel.app",
-// }));
-
 
 app.use(cors({
-    //   origin: [
-    //     "http://localhost:5173",
-    //     "https://recrave-d5ek.vercel.app",
-    //     "https://recrave.vercel.app",
-    //     "https://recrave-nvw9.vercel.app"
-    //   ],
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
 
-// app.options('*', cors());
-
-
-// app.use(express.static(path.join(__dirname, 'project/dist')));
-// app.get(/.*/, (req, res) => {
-//     res.sendFile(path.join(__dirname, 'project/dist', 'index.html'));
-// });
-
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'project/dist')));
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'project/dist', 'index.html'));
 });
 
 
--- project/src/App.tsx
+++ project/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://recrave-nvw9.vercel.app/api/v1/createMessage", {
+      const res = await fetch("http:localhost:5000/api/v1/createMessage", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
