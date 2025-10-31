4c9ce1fd1d3a0a937faf27a20101de5b46fe1045
roxydr <ishikasadhukhan90@gmail.com>
2025-10-31 10:47:43 +0530

    frontend url added

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://recrave-frontend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
